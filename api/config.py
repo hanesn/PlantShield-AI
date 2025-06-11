382751c1a6978b3ce358859309fa3237c77ebae1
Sagnik <sagnikskb0nyp437@gmail.com>
2025-06-11 10:51:14 +0530

    merge branch feature/backend to feature/testing/integration (#17)
    Prepared dataset insights to inform model design.
    
    * feat: complete model training pipeline
    - Built and trained CNN model for plant disease classification
    - Applied class weights for imbalanced dataset
    - Added training/validation accuracy and loss plots
    - Evaluated model on test set and printed metrics
    - Visualized predictions and confusion matrix
    - Implemented callbacks: EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, CSVLogger
    - Saved trained model with versioning
    - Structured directories for logs, checkpoints, saved_models
    
    * chore: restructure project directories and move notebooks and utils
    - Moved training and EDA notebooks into notebooks\ directory
    - Created utils scipts for reusable code
    - Updated import paths and related configs
    
    * feat: Add LR plots and detailed training diagnostics
    - Plotted learning rate vs. epoch
    - Added LR vs. val loss correlation
    - Added plots for true vs. predicted label distribution
    - Identified top 5 epochs with highest val loss improvement
    - Flagged epochs with >20% increase in val loss
    
    * chore: Add .vscode and __pycache__ to .gitignore
    - Prevents unnecessary IDE and Python cache files from being tracked
    
    * feat: add inference notebook, utilities and test images
    - Utility functions can later be reused in API/backend
    - Sets the ground for potential CI test image predictions
    
    * test: add unit tests for utility functions and test configuration setup
    - Added initial unit tests for core utility modules:
      - config.py
      - dataset_utils.py
      - eda.py
      - inference_utils.py
      - train_utils.py
    - Introduced `pytest.ini` to configure test discovery and test root
    - Added `__init__.py` files to treat `training` and `utils` directories as packages, enabling clean imports during testing
    
    * chore: Add .pytest_cache to .gitignore
    
    * Add models.config file for tf serving configurations

--- api/config.py
+++ api/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 USE_TF_SERVING = os.getenv("USE_TF_SERVING", "False") == "True"
 MODEL_DIR = os.getenv("MODEL_DIR", "saved_models")
 MODEL_PATTERN = os.getenv("MODEL_PATTERN", "tomato_model-v*.keras")
-TF_SERVING_URL = os.getenv("TF_SERVING_URL", "http://localhost:8501/v1/models/potatoes_model:predict")
+TF_SERVING_PREDICT_URL = os.getenv("TF_SERVING_PREDICT_URL", "http://localhost:8501/v1/models/tomatoes_model:predict")
+TF_SERVING_HEALTH_URL = os.getenv("TF_SERVING_HEALTH_URL", "http://localhost:8501/v1/models/tomatoes_model")
 LOG_FILE = os.getenv("LOG_FILE", "logs/api.log")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
